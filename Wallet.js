903de51de38fbad3344e44e41a6268fcb02edce1
Mkomarr <48152102+Mkomarr@users.noreply.github.com>
2019-04-09 13:43:02 +0200

    Create Wallet.js

---
+++ Wallet.js
@@ -0,0 +1,31 @@
+
+class Wallet {
+ constructor(money) {
+  let _money = money;
+
+//pobieranie aktualnej zawartości portfela
+  this.getWalletValue = () => _money;
+
+//sprawdzanie czy użytkownik ma odp. ilość środków
+  this.checkCanPlay = value => {
+      if (_money >= value) return true;
+      return false;
+  }
+
+
+//zmiana zawartości portfela po każdej grze
+  this.changeWallet = (value, type = '+') => {
+    if (typeof value === 'number' && !isNaN(value)) {
+       if (type === '+') {
+           return _money += value;
+       } else if (type === '-') {
+           return _money -= value;
+      } else {
+         throw new Error('nieprawidłowy typ działania')
+      }
+  } else {
+      console.log(typeof value);
+      throw new Error('nieprawidłowa liczba');
+  }
+ }
+ }};
